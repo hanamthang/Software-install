1ffc0681648e83333d73f6889ece990a5e48d0ad
hanamthang <hanamthang@gmail.com>
2021-03-04 22:32:35 +1300

    Update Update QGIS GRASS GIS in Windows.py

--- Update QGIS GRASS GIS in Windows.py
+++ Update QGIS GRASS GIS in Windows.py
@@ -1,8 +1,8 @@
 # Updtae QGIS and GRASS GIS using OSGEO4 in Windows 
 1. Open OSGEO4W shell and run:
 set __COMPAT_LAYER=RUNASINVOKER
-2. CD to C:\Users\hnt7\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OSGeo4WThang
+2. CD to C:\Users\....\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OSGeo4WThang
 3. Run Setup 
-4. Press Next to update QGIS and GRASS GIS 
+4. Press Next to update QGIS and GRASS GIS to the desired version 
 
 
